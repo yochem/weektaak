4dc28e6a0e975951f5e76971db6d8a1fd0a31acf
Yochem van Rosmalen <git@yochem.nl>
2024-10-28 19:19:35 +0100

    Restructure project

---
+++ public/script.js
@@ -0,0 +1,132 @@
+function addDays(date, days) {
+  var result = new Date(date);
+  result.setDate(result.getDate() + days);
+  return result;
+}
+
+Date.prototype.isodate = function() {
+    return this.toISOString().split('T')[0];
+}
+
+function setContent(element, content) {
+  document.getElementById(element).innerHTML = content;
+}
+
+function fillWeekList(weekList) {
+  const personalLink = (name) => `<a href="/persoonlijk.html?naam=${name}">${name}</a>`;
+
+  const linkList = [
+    weekList.k1,
+    weekList.k2,
+    weekList.k3,
+  ].map(personalLink).join(', ');
+
+  setContent("kitchen", linkList);
+  setContent("toilet", personalLink(weekList.wc));
+  setContent("shower", personalLink(weekList.douches));
+  setContent("weeknumber", `Week ${weekNumberFromDate(weekList.weekStart)}`);
+
+  const nextMonday = addDays(weekList.weekStart, 7);
+  const nextURL = new URL(window.location.href);
+  nextURL.searchParams.set('date', nextMonday.isodate());
+  document.getElementById("nextWeek").setAttribute('href', nextURL.href);
+
+  const prevMonday = addDays(weekList.weekStart, -7);
+  const prevURL = new URL(window.location.href);
+  prevURL.searchParams.set('date', prevMonday.isodate());
+  document.getElementById("prevWeek").setAttribute('href', prevURL.href);
+}
+
+function weekNumberFromDate(datestring) {
+  let date = new Date(datestring);
+  date.setHours(0, 0, 0, 0);
+  date.setDate(date.getDate() + 3 - (date.getDay() + 6) % 7);
+  let week1 = new Date(date.getFullYear(), 0, 4);
+  return 1 + Math.round(((date.getTime() - week1.getTime()) / 86400000 - 3 + (week1.getDay() + 6) % 7) / 7);
+}
+
+function getShownWeek() {
+  const params = new URLSearchParams(window.location.search);
+  let date = params.get('date') || new Date();
+
+  monday = new Date(date);
+  monday.setDate(monday.getDate() - (monday.getDay() + 6) % 7);
+
+  // Monday is day 1 (and Sunday 0 lol)
+  if (monday.getDay() != 1) {
+    console.error(`${monday.toLocaleDateString('nl-NL')} not a monday`);
+  }
+  return monday.isodate();
+}
+
+function fillPersonalPage(data) {
+  const names = [...new Set(
+    Object.values(data)
+      .flatMap(dateObj => Object.values(dateObj))
+  )];
+
+  var element = document.getElementById("names");
+  for (let name of [...names].sort()) {
+      element.add(new Option(name));
+  }
+
+  const params = new URLSearchParams(window.location.search);
+  let person = params.get("naam");
+  const option = Array.from(element.options).find(opt => opt.value === person);
+  if (option) {
+    element.value = person;
+    fillPersonalTable(data, person);
+  }
+}
+
+function fillPersonalTable(data, person) {
+  const rows = Object.entries(data).reduce((acc, [weekStart, weekData]) => {
+    const weekTasks = Object.entries(weekData)
+      .filter(([_, name]) => name === person)
+      .map(([task, _]) => task.startsWith("k") ? "keuken" : task);
+
+    if (weekTasks.length > 0) {
+      const weekNum = weekNumberFromDate(weekStart);
+      const mondayDate = new Date(weekStart).toLocaleDateString('nl-NL', {
+        month: 'short',
+        day: 'numeric',
+      });
+
+      let cont = `<tr>
+        <td>${weekNum}</td>
+        <td>${mondayDate}</td>
+        <td>${weekTasks.join(" & ")}</td>
+      </tr>`;
+      acc.push(cont);
+    }
+
+    return acc;
+  }, []);
+
+  document.querySelector("#table tbody").innerHTML = rows.join("");
+
+  document.querySelector("#icslink").setAttribute(
+    'href',
+    `/cal/${person.toLowerCase()}.ics`,
+  );
+}
+
+fetch('data.json')
+  .then(response => response.json())
+  .then(data => {
+    let page = new URL(location.href).pathname;
+    console.log({page});
+    if (!page || page === '/' || page === '/index.html') {
+      const monday = getShownWeek();
+      let date = data[monday];
+      date.weekStart = monday;
+      fillWeekList(date);
+    } else if (page === '/persoonlijk.html') {
+      fillPersonalPage(data);
+      document.getElementById("names").addEventListener("change", change => {
+        var opt = change.srcElement.options[change.srcElement.selectedIndex];
+        fillPersonalTable(data, opt.text);
+      });
+    }
+  })
+  .catch(error => console.error('Error:', error));
